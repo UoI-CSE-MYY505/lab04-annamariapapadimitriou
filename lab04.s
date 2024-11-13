3b5f4f63054c79ac2b67367d1a7a4835713c69f7
annamariapapadimitriou <cs05481@uoi.gr>
2024-11-13 20:21:47 +0200

    Update lab04.s

--- lab04.s
+++ lab04.s
@@ -45,5 +45,50 @@ recCheck:
 #---------
 # Write the subroutine code here
 #  You may move jr ra   if you wish.
+    addi sp, sp, -12            # adjust stack for 3
+    sw ra, 8(sp)
+    sw a1, 0(sp)
+    sw a0, 4(sp)
+                                # get first two words of current array
+    lw a1, 0(a0)
+    lw a0, 4(a0)
+    
+    jal str_ge                  # check the two words for correct order
+    addi t1, a0, 0              # save value of comparison to t1 
+    lw ra, 8(sp)                # restore values
+    lw a0, 4(sp)
+    lw a1, 0(sp)
+    bne t1, zero, L1            # check if array is not in order
+    j exit_zero
 #---------
-            jr   ra
+    
+L1:
+    addi a1, a1, -1             # make new size = size - 1 and new array = array - first element
+    addi a0, a0, 4
+    li t2, 1                    # check if size == 0 or size == 1
+    beq a1, t2, exit_one     
+    beq a1, zero, exit_one
+
+    jal recCheck                # recur
+
+                                # restore values to starting except a0 and adjust stack
+    lw ra, 8(sp)
+    lw a1, 0(sp)
+    addi sp, sp, 12
+  
+    jr ra                       # return to caller
+#---------
+    
+exit_one:
+    addi a0, zero, 1            # value 1 to a0
+    addi sp, sp, 12             # adjust stack
+    jr ra                       # return to caller
+#---------
+
+exit_zero:
+    addi a0, zero, 0            # value 0 to a0
+    addi sp, sp, 12             # adjust stack
+    jr ra                       # return to caller
+#  You may move jr ra   if you wish.
+#---------
+            
